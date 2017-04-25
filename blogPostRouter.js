dc7d9a80f7085692f544302b0748f812a7781e91
cnsheafe <cnsheafe@gmail.com>
2017-04-25 16:08:00 -0500

    added rest of endpoint handlers

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -6,82 +6,72 @@ const {DATABASE_URL} = require('./config');
 
 const jsonParser = bodyParser.json();
 const router = express.Router();
-// mongoose.Promise = global.Promise;
 
-// mongoose.connect(DATABASE_URL);
+
 router.get('/', (req, res) => {
-	blogPost.find().exec().then(allPosts => {
+	blogPost.find().exec().
+	then(allPosts => {
 		res.status(200).json({
 			allPosts: allPosts.map(post => post.showPost())
 		});
 	});
 });
 
-router.get('/:id', function (req, res) {
-	const post = blogPost.get(req.params.id);
-	if( post.constructor === Array ) {
-		const msg = `Blog post with id:${req.params.id} does not exist`;
-		console.info(msg);
-		return res.status(404).send(msg);
-	}
-	return res.status(200).json(post);
+router.get('/:id', (req, res) => {
+	blogPost.findById(req.params.id).exec()
+	.then( post => res.json(post.showPost()) )
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({errMsg: 'Internal server error'});
+	});
 });
+
 
 router.post('/', jsonParser, function(req, res) {
 	const requiredFields = ['title', 'content', 'author'];
 	let field = '';
 	for (let i = 0; i < requiredFields.length; i++) {
 		field = requiredFields[i];
-		console.log(req.body);
 		if (!(field in req.body)){
 			const msg = `${field} not found in body`;
 			return res.status(400).send(msg);
 		}
 	}
-	let date = null;
-	if (typeof req.body.publishDate !== 'undefined') {
-		date = req.body.publishDate;
-	}
-
-	const post = blogPost.create(req.body.title, req.body.content, req.body.author, date);
-	res.status(201).json(post);
+	blogPost.create({
+		title: req.body.title,
+		author: {
+			firstName: req.body.author.firstName,
+			lastName: req.body.author.lastName
+		},
+		content: req.body.content,
+		created: Date.now()
+	})
+	.exec()
+	.then( post => res.status(201).json(post.showPost()) )
+	.catch(err => {
+		console.error(err);
+		res.status(500).json({errMsg: 'Internal server error'});
+	});
 });
 
 router.put('/:id', jsonParser, function(req, res) {
-	console.log(req.params.id);
-	const requiredFields = ['title', 'content', 'author'];
-	let field = '';
-	for (let i = 0; i < requiredFields.length; i++) {
-		field = requiredFields[i];
-		if (!(field in req.body)){
-			const msg = `${field} not found in body`;
-			return res.status(400).send(msg);
-		}
-	}
-	for (let i = 0; i < blogPost.posts.length; i++) {
-		if(req.params.id === blogPost.posts[i].id) {
-			break;
+	const updateableFields = ['title', 'author', 'content'];
+	const toUpdate = {};
+	updateableFields.forEach(field => {
+		if (field in req.body) {toUpdate[field] = req.body[field];}
+	});
 
-		}
-		else {
-			const msg = `${req.params.id} does not exist`;
-			return res.status(400).send(msg);
-		}
-	}
-	res.status(204).json(blogPost.update(req.body));
+	blogPost.findByIdAndUpdate(req.params.id, {$set: toUpdate})
+	.exec()
+	.then(post => res.status(204).end())
+	.catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 router.delete('/:id', function(req,res) {
-	for (let i = 0; i < blogPost.posts.length; i++) {
-		if(req.params.id === blogPost.posts[i].id) {
-			break;
-		}
-		else {
-			const msg = `${req.params.id} does not exist`;
-			return res.status(400).send(msg);
-		}
-	}
-	res.status(204).json(blogPost.delete(req.params.id));
+	blogPost.findByIdAndRemove(req.params.id)
+	.exec()
+	.then(post => res.status(204).end())
+	.catch(err => res.status(500).json({message: 'Internal server error'}));
 });
 
 module.exports = router;
